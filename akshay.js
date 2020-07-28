ee9825cf7fdde8678bcbfe227b3602c8a51cc2d1
akshayallam <31719865+akshayallam@users.noreply.github.com>
2020-07-27 18:30:39 -0700

    Delete akshay.js

--- akshay.js
+++
@@ -1,9 +0,0 @@
-function submitClick(e)
-{
-     e.preventDefault();
-     $("#contactForm").slideUp("slow")', 2000);
-}
-
-$(document).ready(function() {
-    $('#textbox').click(submitClick);
-});
